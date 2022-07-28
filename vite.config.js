1723089827702d370b403423d13a38060e8f8852
涂威 <tuwei@lonch.com.cn>
2022-07-28 15:15:26 +0800

    配置文件修改

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+/*
+ * @Author: tuWei
+ * @Date: 2022-07-04 17:47:02
+ * @LastEditors: tuWei
+ * @LastEditTime: 2022-07-28 15:14:26
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: '/chat/',
+  build: {
+    outDir: 'chat',
+    assetsInlineLimit: 0
+  },
+  plugins: [vue()],
 })
