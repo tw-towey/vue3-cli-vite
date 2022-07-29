a44d4868d57048b6ec6c6ee358914ce0bb7d3808
刘萌迪 <liumd@tongtech.com>
2022-07-29 16:59:13 +0800

    修改气泡小三角

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -7,12 +7,24 @@
 <template>
   <div class="p-5">
     <ul :style="{'height': '80%', 'overflow': 'auto'}" ref="myRef">
-      <li v-for="item in ListData.list" :key="item" :class="style(item,'li')">
-        <p class="flex items-center" :class="style(item,'p')">
-          <span class="inline-block px-3 py-1 bg-blue-400 text-white text-center font-extrabold rounded-md" :class="style(item,'username')">{{item.username}}</span>
-          <span class="ml-5 mr-5 w-50 text-gray-300" v-if="item.createdAt">{{ new Date(item.createdAt).toLocaleString() }}</span>
-        </p>
-        <span class="inline-block text-left bg-purple-100 text-sm px-5 py-2 mx-3 my-3 rounded-md font-semibold text-purple-500 bg-violet-200 ring ring-violet-300 ring-offset-2 ring-offset-violet-50">{{item.content}}</span>
+      <li v-for="item in ListData.list" :key="item" class="p-1 flex" :class="style(item,'li')">
+        <div class="rounded-full h-16 w-16 min-w-16 bg-blue-400 bg-cover avatar" :style="{'background-image':`url(${item.avatar})`}"></div>
+        <div class="flex flex-col text-left" :class="style(item,'div')">
+          <span class="ml-3 mr-3 w-50 text-gray-300">
+            <span class="mr-3 text-gray-400">{{item.username}}</span>
+            {{ new Date(item.createdAt).toLocaleString() || '' }}
+          </span>
+          <div class="relative">
+            <span class="inline-block text-left text-sm px-5 py-2 mx-3 my-3 rounded-md font-semibold ring ring-offset-2" :class="style(item,'content')">
+              {{item.content}}
+            </span>
+            <span class="sjBox absolute top-5" :class="style(item,'sj')">
+              <span class="inline-block absolute w-0 h-0 -top-0.5 left-0.5" :class="style(item,'sjspan')"></span>
+              <span class="inline-block absolute w-0 h-0" :class="style(item,'sjspan')"></span>
+              <span class="inline-block absolute w-0 h-0" :class="style(item,'sjspan')"></span>
+            </span>
+          </div>
+        </div>
       </li>
     </ul>
     <div class="pt-5 pb-5">
@@ -96,19 +108,73 @@ const style = (item,tag)=> {
   let self = item.userId===form.userId;
   if( tag === 'li' ) {
     return {
-      'p-1': true,
-      'text-right': self,
+      'flex-row-reverse': self,
       'ml-60': self,
       'mr-60': !self
     }
-  }else if( tag === 'username' ) {
+  }else if( tag === 'div' ) {
     return {
-      'bg-red-400': self
+      'text-right': self
     }
-  }else if( tag === 'p' ) {
+  }else if( tag === 'content' ) {
     return {
-      'flex-row-reverse': self
+      'text-violet-500': self,
+      'bg-violet-200': self,
+      'ring-violet-300': self,
+      'ring-offset-violet-50': self,
+      'text-blue-500': !self,
+      'bg-blue-200': !self,
+      'ring-blue-300': !self,
+      'ring-offset-blue-50': !self
+    }
+  }else if( tag === 'sj' ) {
+    return {
+      'right-3': self,
+      '-left-4': !self
+    }
+  }else if( tag === 'sjspan' ) {
+    return {
+      'sjleft': !self,
+      'sjright': self
     }
   }
 }
 </script>
+
+<style scoped>
+.avatar {
+  min-width: 4rem;
+}
+.sjBox span:nth-child(1) {
+  border: 0.75rem solid transparent;
+}
+.sjBox span:nth-child(2) {
+  border: 10px solid transparent;
+}
+.sjBox span:nth-child(3) {
+  border: 8px solid transparent;
+}
+.sjBox span:nth-child(1).sjleft {
+  border-right-color: rgb(147 197 253);
+}
+.sjBox span:nth-child(1).sjright {
+  border-left-color: rgb(196 181 253);
+}
+.sjBox span:nth-child(2).sjleft {
+  border-right-color: #eff6ff;
+  left: 0.5rem;
+}
+.sjBox span:nth-child(2).sjright {
+  border-left-color: #f5f3ff;
+}
+.sjBox span:nth-child(3).sjleft {
+  border-right-color: #bfdbfe;
+  left: 0.8rem;
+  top: 2px;
+}
+.sjBox span:nth-child(3).sjright {
+  border-left-color: #ddd6fe;
+  left: -0.1rem;
+  top: 2px;
+}
+</style>
